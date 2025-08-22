33761f7d64f1f4f454627aabf6a1d6dee18c508b
Emeline-Wulstecke <emeline.wulstecke@gmail.com>
2025-08-22 14:20:53 +0200

    chore: configure assets and project structure

---
+++ src/icons.ts
@@ -0,0 +1,10 @@
+// LIB
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// SOLID
+import { faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+// BRANDS
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faUser, faEnvelope, faGithub, faLinkedin)
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+// VUE
 import { createApp } from 'vue'
+
+// APP
 import App from './App.vue'
+
+// ROUTER
 import router from './router'
 
-const app = createApp(App)
+// STYLES
+import './assets/tailwind.css'
 
+// FONTAWESOME
+import './icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const app = createApp(App)
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
